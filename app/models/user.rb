d763dcad3e290b8bd299cef64eb0f773abd327f6
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-06 17:37:52 +0900

    ユーザーモデルのvalidationの再編

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, presence: true, format: { with: PASSWORD_REGEX}
          
   with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    NAME_ZENKAKU = /\A[ぁ-んァ-ヶ一-龥々]+\z/
+    NAME_KATAKANA = /\A[\p{katakana}\p{blank}ー－]+\z/
     validates :name             
-    validates :last_name        
-    validates :first_name       
-    validates :last_name_kana,  format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-    validates :first_name_kana, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :password, format: { with: PASSWORD_REGEX}
+    validates :last_name, format: {with: NAME_ZENKAKU}
+    validates :first_name, format: {with: NAME_ZENKAKU}
+    validates :last_name_kana,  format: {with: NAME_KATAKANA}
+    validates :first_name_kana, format: {with: NAME_KATAKANA}
     validates :birthday
   end
 end
