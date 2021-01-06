14a253df0991a051ba7d26427e1f4bf632c65518
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-06 16:21:13 +0900

    with_optionsでのコード再編

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,12 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates :password, presence: true, format: { with: PASSWORD_REGEX}
          
-  
-  validates :name,             presence: true
-  validates :last_name,        presence: true
-  validates :first_name,       presence: true
-  validates :last_name_kana,   presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :first_name_kana,  presence: true, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :birthday,         presence: true
+  with_options presence: true do
+    validates :name             
+    validates :last_name        
+    validates :first_name       
+    validates :last_name_kana,  format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :first_name_kana, format: {with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+    validates :birthday
+  end
 end
