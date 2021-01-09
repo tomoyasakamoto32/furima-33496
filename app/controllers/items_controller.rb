7108588189996e29152dcaddb8e0fba978a6f770
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-01-09 17:39:55 +0900

    重複する記述をプライベートメソッドに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :root_to_path, only: :edit
+  before_action :item_find_params, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,5 +46,9 @@ class ItemsController < ApplicationController
     if current_user.id != @item.user.id
       redirect_to root_path
     end
+  end
+
+  def item_find_params
+    @item = Item.find(params[:id])
   end
 end
